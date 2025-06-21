427f305fba305c821dc5a7806501b3195b5feeb4
syntax <dlscam112@gmail.com>
2025-06-21 18:14:18 +0500

    ща буду менять цветавую гамму!

--- frontend/src/components/molecules/heaaderCurrentChat.tsx
+++ frontend/src/components/molecules/heaaderCurrentChat.tsx
@@ -6,48 +6,41 @@ import { currentChatDataStore } from '../../app/store/chatStore/currentChatDataS
 import { handleCloseCurrentChat } from '../../shared/utils/handlers/uiHandlers'
 import Ava from '../atoms/ava'
 
- function HeaderCurrentChat(){
-
+function HeaderCurrentChat() {
 	const friendsJSON = toJS(statusFriendsStore)
 	const dataJSON = toJS(currentChatDataStore)
 
 	const isFriend = friendsJSON.chats.find(
-		friend => 
-			friend.username === dataJSON.username
-	) 
-	
+		friend => friend.username === dataJSON.username
+	)
+
 	return (
-		<div className='w-[100%] h-[75px] 
-		flex items-center px-[3px] md:border border-[#535252] rounded-full headerCurrentChat'>
+		<div
+			className='w-[100%] h-[75px] 
+		flex items-center px-[3px] md:border border-[#535252] rounded-full headerCurrentChat'
+		>
+			<div
+				className='hidden_el cursor-pointer'
+				onClick={handleCloseCurrentChat}
+			>
+				<FaArrowLeft color='#fff' size={16} />
+			</div>
 
-		<div 
-		className='hidden_el cursor-pointer'
-		onClick={handleCloseCurrentChat}>
-			<FaArrowLeft color='#fff' size={16} />
-		</div>
-				
-				{/* AVA */}
-				<div className='w-[50px] h-[50px] _bg-white/30 rounded-[50%] ml-2 mr-3 flex justify-center items-center border-[#333]'>
-						
-				<Ava 
-					ava={currentChatDataStore.ava} 
-					username={
-					currentChatDataStore.username
-					} 
+			{/* AVA */}
+			<div className='w-[50px] h-[50px] _bg-white/30 rounded-[50%] ml-2 mr-3 flex justify-center items-center border-[#333]'>
+				<Ava
+					ava={currentChatDataStore.ava}
+					username={currentChatDataStore.username}
 				/>
+			</div>
 
-				</div>
- 
-				<div className='ml-3'>
-					<p>{currentChatDataStore.username}</p>
-					
+			<div className='ml-3'>
+				<p>{currentChatDataStore.username}</p>
+
 				<div className='flex items-center'>
 					<p>{isFriend?.status || 'offline'}</p>
 				</div>
-
-				</div>
-
-
+			</div>
 		</div>
 	)
 }
