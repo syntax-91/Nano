686d3c2d70d6f03bcd225b0ab0b7ee8eacc03dd3
syntax <dlscam112@gmail.com>
2025-06-25 13:17:05 +0500

    добавил иконки для меню в Desktop

--- frontend/src/components/organisms/menu/Desktop/menuDesktop.tsx
+++ frontend/src/components/organisms/menu/Desktop/menuDesktop.tsx
@@ -1,5 +1,9 @@
 import clsx from 'clsx'
 import { observer } from 'mobx-react-lite'
+import { CgProfile } from 'react-icons/cg'
+import { CiLogout } from 'react-icons/ci'
+import { FcAbout } from 'react-icons/fc'
+import { MdDarkMode, MdLightMode, MdSettings } from 'react-icons/md'
 import { configStore } from '../../../../app/store/app/configStore'
 import { isOpenStore } from '../../../../app/store/isOpen/isOpenSettingsStore'
 import { handleSettings } from '../../../../shared/utils/handlers/uiHandlers'
@@ -32,39 +36,47 @@ function MenuDesktop() {
 		>
 			<div
 				className='flex justify-center 
- 		py-4 px-6 hover:bg-white/5 rounded-2xl font-light text-2xl'
+ 				py-3 hover:bg-white/5 rounded-2xl font-light text-2xl items-center gap-2'
 			>
+				<CgProfile size={20} />
 				<h3>profile</h3>
 			</div>
 
 			<div
 				className='flex justify-center 
- 		py-3  hover:bg-white/5 rounded-2xl font-light text-2xl'
+ 				py-3  hover:bg-white/5 rounded-2xl font-light text-2xl items-center gap-1'
 				onClick={handleSettings}
 			>
+				<MdSettings size={20} />
 				<h3>settings</h3>
 			</div>
 
 			<div
 				className='flex justify-center 
- 		py-3  hover:bg-white/5 rounded-2xl font-light text-2xl'
+ 				py-3  hover:bg-white/5 rounded-2xl font-light text-2xl items-center gap-1'
 			>
+				<FcAbout />
 				<h3>about</h3>
 			</div>
 
 			<div
 				className='flex justify-center 
- 		py-3  hover:bg-white/5 rounded-2xl font-light text-2xl'
+ 				py-3  hover:bg-white/5 rounded-2xl font-light text-2xl items-center gap-1'
 				onClick={toggleTheme}
 			>
-				<h3>{configStore.currentTheme} mode</h3>
+				{configStore.currentTheme == 'dark' && <MdDarkMode size={25} />}
+
+				{configStore.currentTheme == 'light' && <MdLightMode size={25} />}
+
+				<h2>{configStore.currentTheme}</h2>
 			</div>
 
 			<div
 				className='flex justify-center 
- 			py-4  hover:bg-white/5 rounded-2xl font-light text-2xl'
+ 				py-4  hover:bg-white/5 rounded-2xl font-light text-2xl items-center gap-1'
 				onClick={handleLogOut}
 			>
+				<CiLogout size={20} />
 				<h3>logOut</h3>
 			</div>
 		</div>
