ea949d6c416f48bfddcad5d83026d96856c9404a
syntax <dlscam112@gmail.com>
2025-06-25 13:00:58 +0500

    какая-то ошибка в giit, пришлось удалить и скачать репо, придётся заново писать меню для телефона т.п

--- frontend/src/components/atoms/ava.tsx
+++ frontend/src/components/atoms/ava.tsx
@@ -4,39 +4,37 @@ import { observer } from 'mobx-react-lite'
 import { statusFriendsStore } from '../../app/store/app/statusUsers'
 
 interface IAva {
-	ava: string,
-	w?: number,
-	h?: number,
+	ava: string
+	w?: number
+	h?: number
 	username?: string
 }
 
- function Ava (
-	{ ava, w = 15, h = 15, ...rest }:IAva
-){
-
+function Ava({ ava, w = 15, h = 15, ...rest }: IAva) {
 	const clsAVA = `ava  rounded-[50%]           w-[100%] h-[100%] flex items-center relative justify-center border-[#464545] shrink-0 grow-0`
 
 	const friendsJSON = toJS(statusFriendsStore)
 
-
-		const isFriend = friendsJSON.chats.find(
-			friend => rest.username === friend.username
-		) 
+	const isFriend = friendsJSON.chats.find(
+		friend => rest.username === friend.username
+	)
 
 	return (
-		<div className={clsx(clsAVA, 
-		`bg-gradient-to-r flex shrink-0
-		 from-white/5 to-white/15`)}>
-			
-			{ava.length > 0 && 
-				<img src={ava} alt="" />}
-
-			<div 
+		<div
 			className={clsx(
-				'w-[10px] h-[10px] absolute right-1 bottom-[3px]',
-			 	isFriend?.status
+				clsAVA,
+				`bg-gradient-to-r flex shrink-0
+		 from-white/5 to-white/15`
 			)}
-		/>
+		>
+			{ava.length > 0 && <img src={ava} alt='' />}
+
+			<div
+				className={clsx(
+					'w-[10px] h-[10px] absolute right-1 bottom-[3px]',
+					isFriend?.status
+				)}
+			/>
 		</div>
 	)
 }
