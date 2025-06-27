d556a648cdbc3a069b4f5e2909878f0461a08191
syntax <dlscam112@gmail.com>
2025-06-27 18:24:08 +0500

    fix: адаптировал screenLoader под темы

--- frontend/src/components/organisms/screen/Loaders/ScreenBlock.tsx
+++ frontend/src/components/organisms/screen/Loaders/ScreenBlock.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx'
+import { configStore } from '../../../../app/store/app/configStore'
 
 interface props {
 	location: 'left' | 'right'
@@ -16,7 +17,10 @@ export function ScreenBlock({ location, w = 290 }: props) {
 		>
 			<div
 				style={{ width: w }}
-				className='bg-white/10 h-12 my-5 rounded-full relative flex items-center justify-cent_er animate-pulse max-w-[80%]'
+				className={clsx(
+					' h-12 my-5 rounded-full relative flex items-center justify-cent_er animate-pulse max-w-[80%]',
+					configStore.currentTheme == 'dark' ? 'bg-white/10' : 'bg-black/10'
+				)}
 			>
 				<div
 					className='w-[100%] rounded-4xl flex items-center bg-whit/20 ws 
