ed50615e01e15c94b8d098fb8f3abcce3d5e9685
syntax <dlscam112@gmail.com>
2025-06-27 18:47:53 +0500

    feature: адаптировал 404 под светлую

--- frontend/src/pages/_404/_404.tsx
+++ frontend/src/pages/_404/_404.tsx
@@ -1,8 +1,28 @@
+import clsx from 'clsx'
+import { useNavigate } from 'react-router-dom'
+import { configStore } from '../../app/store/app/configStore'
+
 export default function NotFound() {
+	const n = useNavigate()
+
+	const clsMC =
+		'w-[100%] h-[100vh] flex justify-center items-center font-bold flex-col'
 
 	return (
-		<>
-			Not Found Page
-		</>
+		<div
+			className={clsx(
+				clsMC,
+				configStore.currentTheme == 'dark' ? 'dark' : 'light'
+			)}
+		>
+			<h2 className='text-2xl'>Not Found Page</h2>
+
+			<p>
+				go to{' '}
+				<span className='cp' onClick={() => n('/')}>
+					Home
+				</span>
+			</p>
+		</div>
 	)
 }
