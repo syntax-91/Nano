c6f4c86d50827180ef50ea0cb860b53162b9d1d0
syntax <dlscam112@gmail.com>
2025-06-29 23:13:54 +0500

    featue: добавил возможность ставить аву

--- frontend/src/app/store/app/userData.ts
+++ frontend/src/app/store/app/userData.ts
@@ -1,14 +1,21 @@
-import Cookies from 'js-cookie'
+import cs from 'js-cookie'
 import { makeAutoObservable } from 'mobx'
 
 class userDataStoreClass {
-	
-	userName = Cookies.get('userName_nano')
-	isAuth = Cookies.get('isAuth_nano')||false
+	userName = cs.get('userName_nano')
+	isAuth = cs.get('isAuth_nano') || false
 
+	dataMap = {
+		ava: cs.get('ava_nano') || '',
+	}
 
 	constructor() {
 		makeAutoObservable(this)
+	}
+
+	setUserData(type: keyof typeof this.dataMap, value: string) {
+		this.dataMap[type] = value
+		cs.set(`${type}_nano`, value)
 	}
 }
 
---
+++ frontend/src/components/organisms/Settings/components/changeAva.tsx
@@ -0,0 +1,57 @@
+import { observer } from 'mobx-react-lite'
+import { useState } from 'react'
+import socket from '../../../../app/socket/socket'
+import { configStore } from '../../../../app/store/app/configStore'
+import { userDataStore } from '../../../../app/store/app/userData'
+import Ava from '../../../atoms/ava'
+import { Button } from '../../../atoms/Button'
+import { Input } from '../../../atoms/Input'
+
+function ChangeAva() {
+	const clsMC = 'fn-3 mx-auto flex justify-center items-center'
+
+	const [url, setUrl] = useState('')
+
+	const clsC = ''
+
+	const handleChangeAva = () => {
+		userDataStore.setUserData('ava', url)
+
+		socket.emit('change', {
+			type: 'ava',
+			url: url,
+			username: userDataStore.userName,
+		})
+	}
+
+	return (
+		<div className={clsMC}>
+			{/* container */}
+			<div className='borderP w-[50%] rounded-2xl p-2'>
+				{/* AVA */}
+				<div className='w-20 h-20 mx-auto my-8  rounded-full'>
+					<Ava ava={userDataStore.dataMap.ava} />
+				</div>
+
+				<Input
+					rhf={false}
+					placeholder='Enter url (base64)'
+					value={url}
+					onChange={e => setUrl(e.target.value)}
+					theme={configStore.currentTheme == 'dark' ? 'dark' : 'light'}
+					style='full'
+				/>
+
+				<Button
+					disabled={!url}
+					label='submit'
+					w={400}
+					max_w={100}
+					onClick={handleChangeAva}
+				/>
+			</div>
+		</div>
+	)
+}
+
+export default observer(ChangeAva)
