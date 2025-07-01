d66ccf31885a90d3b648b82d2cbdd2917fd42aa2
syntax <dlscam112@gmail.com>
2025-07-02 00:07:35 +0500

    HUI: ебаать SQLite имба, другие БД сосут нахуй, скорость 10/10, удоство 10/10, 1000+ social credit, 10 кошко жена, партия гордится мной

---
+++ backend/src/prisma/migrations/20250701190017_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_password_key" ON "User"("password");
